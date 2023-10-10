c77cae9f846683818b0dad5569ae616ebae210ba
Erhan İkinci <erhanikinci61@gmail.com>
2023-10-10 14:07:35 +0300

    created login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import Input from "components/Iput";
+import { useEffect, useState } from "react";
+import { useRef } from "react";
+import { AiFillFacebook } from "react-icons/ai";
 
 function App() {
+  const ref = useRef();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const enable = username && password;
+
+  useEffect(() => {
+    let images = ref.current.querySelectorAll("img"),
+      total = images.length,
+      current = 0;
+    const imageSlider = () => {
+      if (current > 0) {
+        images[current - 1].classList.add("opacity-0");
+      } else {
+        images[total - 1].classList.add("opacity-0");
+      }
+      images[current].classList.remove("opacity-0");
+      current += 1;
+
+      if (current === total - 1) {
+        current = 0;
+      } else {
+        current = current + 1;
+      }
+    };
+    imageSlider();
+    let interval = setInterval(imageSlider, 3000);
+    return () => clearInterval(interval);
+  }, [ref]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='h-full w-full flex flex-wrap overflow-auto items-center gap-x-8 justify-center'>
+      <div className='hidden md:block w-[380px] h-[581px] bg-logo-pattern relative bg-[length:468.32px_634.15px] bg-[top_left_-46px] '>
+        <div
+          className='w-[250px] h-[538px] absolute top-[27px] right-[18px] '
+          ref={ref}
         >
-          Learn React
-        </a>
-      </header>
+          <img
+            className='w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear'
+            src='https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot1.png?__d=www'
+            alt=''
+          />
+          <img
+            className='w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear'
+            src='https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot2.png?__d=www'
+            alt=''
+          />
+          <img
+            className='w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear'
+            src='https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot3.png?__d=www'
+            alt=''
+          />
+          <img
+            className='w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear'
+            src='https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot4.png?__d=www'
+            alt=''
+          />
+        </div>
+      </div>
+
+      <div className='w-[350px] grid gap-y-3'>
+        <div className=' bg-white border px-[40px] pt-10 pb-6 '>
+          <a href='#' className='flex justify-center  mb-8'>
+            <img
+              className='h-[51px] '
+              src='https://static.cdninstagram.com/rsrc.php/v3/yS/r/ajlEU-wEDyo.png'
+              alt=''
+            />
+          </a>
+          <form className='grid gap-y-1.5'>
+            <Input
+              type='text'
+              value={username}
+              label='Phone number, username or email address'
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              type='password'
+              value={password}
+              label='Password'
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button
+              disabled={!enable}
+              type='submit'
+              className='h-[30px] mt-1 rounded bg-brand font-medium text-white text-sm disabled:opacity-50 '
+            >
+              Log In
+            </button>
+
+            <div className='flex items-center my-2.5 mb-3.5 '>
+              <div className='h-px bg-gray-300 flex-1' />
+              <span className='px-4 text-[13px] text-gray-500 font-semibold my-2'>
+                OR
+              </span>
+              <div className='h-px bg-gray-300 flex-1' />
+            </div>
+            <a
+              href='#'
+              className='flex justify-center mb-2.5 items-center gap-2 text-sm font-semibold text-facebook'
+            >
+              <AiFillFacebook size={20} /> Log in with Facebook
+            </a>
+            <a
+              href='#'
+              className='text-xs flex items-center justify-center text-link'
+            >
+              Forgotten your password?
+            </a>
+          </form>
+        </div>
+        <div className='bg-white border p-4 text-sm text-center '>Don't have an account? <a href="#" className="font-semibold text-brand">Sign up</a></div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Iput.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+export default function Input({ label, type="text", ...props }) {
+
+  const [show, setShow] = useState(false);
+
+  const [inputType, setType] = useState(type);
+
+  useEffect(() => {
+    if (show) {
+        setType('text');
+    }else if (type === "password") {
+        setType('password');
+    }
+  }, [show])
+
+  return (
+    <label htmlFor='' className='block flex relative bg-zinc-50 border rounded-sm focus-within:border-gray-400'>
+      <input
+        required={true}
+        type={inputType}
+        className='px-2 outline-none text-xs  w-full h-[38px] valid:pt-[10px] peer'
+        {...props}
+      />
+      <small className='absolute top-1/2 left-[9px] cursor-text pointer-events-none text-xs text-gray-400 -translate-y-1/2 transition-all peer-valid:text-[10px] peer-valid:top-2.5'>
+        {label}
+      </small>
+      
+      {type === "password" && props.value && (
+        <button type="button" onClick={() => setShow(show => !show)} className='h-full flex items-center text-sm font-semibold pr-2'>
+            {show ? "Hide" : "Show"}
+        </button>
+      )}
+    </label>
+  );
+}
